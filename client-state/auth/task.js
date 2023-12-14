2c387cfe7cb7c3ab8d8b5a0337aa5b4c20dd30fd
Elizaveta Smekhova <smekhovaelizaveta@gmail.com>
2023-12-14 20:00:32 +0300

    На проверку

---
+++ client-state/auth/task.js
@@ -0,0 +1,30 @@
+const form = document.getElementById('signin__form');
+const signin = document.querySelector('.signin');
+const welcome = document.querySelector('.welcome');
+const userName = welcome.querySelector('#user_id');
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const request = new XMLHttpRequest();
+
+    request.open('Post', 'https://students.netoservices.ru/nestjs-backend/auth');
+    request.responseType = 'json';
+    request.onload = function() {
+        if(request.response.success){
+            const userId = request.response.user_id;
+            localStorage.setItem('userId', userId);
+            signin.classList.remove('signin_active');
+            welcome.classList.add('welcome_active');
+            userName.textContent = userId;
+
+        } else {
+            alert('Неверный логин или пароль')
+        }
+    }
+    request.send(formData);
+    form.reset();
+});
+
